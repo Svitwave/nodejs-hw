be4fb2baa3cbcbe662b9e77af104b72c529ae324
Svitwave <svitlanamaz@gmail.com>
2026-01-14 13:25:07 +0200

    update task

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ import cors from 'cors';
 // import { Note } from '../models/note.js';
 import helmet from 'helmet';
 
-import { connectMongoDB } from '../db/connectMongoDB.js';
-import { logger } from '../middleware/logger.js';
-import { notFoundHandler } from '../middleware/notFoundHandler.js';
-import { errorHandler } from '../middleware/errorHandler.js';
-import notesRouter from '../routes/notesRoutes.js';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRouter from './routes/notesRoutes.js';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
